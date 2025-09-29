ee4e24d2da44aaa2a668fb59c532d72f9f0a78ba
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-09-29 17:46:31 +0200

    feat(users): implement user registration and detail views with serializer validation

---
+++ backend/users/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True, label="Confirm password")
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'email': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        # Validate password strength
+        try:
+            validate_password(attrs['password'], self.instance)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
