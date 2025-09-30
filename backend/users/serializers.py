95bbfe591e1c97950554677ba513a4226c1fe019
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-10-01 01:16:17 +0200

    lint(backend): refactor code for consistency and readability

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,34 +1,39 @@
 from rest_framework import serializers
-from .models import User
+
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 
+from .models import User
+
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
-    password2 = serializers.CharField(write_only=True, required=True, label="Confirm password")
+    password2 = serializers.CharField(
+        write_only=True, required=True, label="Confirm password"
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2', 'email_verified')
-        read_only_fields = ('email_verified',)
-        extra_kwargs = {
-            'email': {'required': True}
-        }
+        fields = ("username", "email", "password", "password2", "email_verified")
+        read_only_fields = ("email_verified",)
+        extra_kwargs = {"email": {"required": True}}
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
 
         # Validate password strength
         try:
-            validate_password(attrs['password'], self.instance)
+            validate_password(attrs["password"], self.instance)
         except ValidationError as e:
             raise serializers.ValidationError(e.messages)
 
         return super().validate(attrs)
 
     def create(self, validated_data):
-        validated_data.pop('password2')
+        validated_data.pop("password2")
         user = User.objects.create_user(**validated_data)
         # Mark email as unverified by default
         user.email_verified = False
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,20 +1,23 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from django.urls import path
+
 from .views import (
-    RegisterView, 
-    UserDetailView, 
-    SendEmailVerificationView, 
-    VerifyEmailView
-)
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+    RegisterView,
+    SendEmailVerificationView,
+    UserDetailView,
+    VerifyEmailView,
 )
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='auth_register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('me/', UserDetailView.as_view(), name='user_detail'),
-    path('send-verification-email/', SendEmailVerificationView.as_view(), name='send_verification_email'),
-    path('verify-email/<str:token>/', VerifyEmailView.as_view(), name='verify_email'),
+    path("register/", RegisterView.as_view(), name="auth_register"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", UserDetailView.as_view(), name="user_detail"),
+    path(
+        "send-verification-email/",
+        SendEmailVerificationView.as_view(),
+        name="send_verification_email",
+    ),
+    path("verify-email/<str:token>/", VerifyEmailView.as_view(), name="verify_email"),
 ]
