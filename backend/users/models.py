dba9c01f7ec84357491f137a7eb65164bae37187
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-09-30 17:25:59 +0200

    feat(users): add email verification functionality and related views

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email_verified = models.BooleanField(default=False)
+    email_verification_token = models.CharField(max_length=64, blank=True, null=True)
 
     def __str__(self):
         return self.username
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import RegisterView, UserDetailView
+from .views import (
+    RegisterView, 
+    UserDetailView, 
+    SendEmailVerificationView, 
+    VerifyEmailView
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,4 +15,6 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserDetailView.as_view(), name='user_detail'),
+    path('send-verification-email/', SendEmailVerificationView.as_view(), name='send_verification_email'),
+    path('verify-email/<str:token>/', VerifyEmailView.as_view(), name='verify_email'),
 ]
