b7e93148927f28c23c2df129aa6f2878f76ea639
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-10-02 01:00:33 +0200

    fix(style): standardize string quotes in project files

--- backend/area_project/celery.py
+++ backend/area_project/celery.py
@@ -9,13 +9,13 @@ sys.path.append(str(BASE_DIR))
 from celery import Celery
 
 # Set the default Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'area_project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "area_project.settings")
 
-app = Celery('area_project')
+app = Celery("area_project")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
-app.config_from_object('area_project.settings', namespace='CELERY')
+app.config_from_object("area_project.settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
@@ -23,4 +23,4 @@ app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')
+    print(f"Request: {self.request!r}")
