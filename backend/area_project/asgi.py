0a2260da65cb39075b13bc112d467054e31d7318
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-10-02 03:09:48 +0200

    refactor(asgi): move routing import to the top for clarity

--- backend/area_project/asgi.py
+++ backend/area_project/asgi.py
@@ -15,13 +15,14 @@ from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
 
+from area_project import routing
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "area_project.settings")
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-from area_project import routing
 
 application = ProtocolTypeRouter(
     {
