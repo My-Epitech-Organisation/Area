06577b83ac0009f4fe5f5f1a81378dc697213569
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-10-06 20:57:26 +0200

    feat(dashboard): refactor components and styles for improved layout and readability

--- frontend/src/components/dashboard/KPIPlaceHolder.tsx
+++ frontend/src/components/dashboard/KPIPlaceHolder.tsx
@@ -9,9 +9,9 @@ import React from "react";
 
 const KPIPlaceHolder: React.FC = () => {
   return (
-    <section className="w-3/4 mb-10 text-center">
-      <div className="bg-white/10 border border-white/10 rounded-2xl py-10 text-gray-400 text-lg italic">
-        <h2 className="text-2xl mb-4 font-semibold text-indigo-300">KPI's</h2>
+    <section className="kpi-placeholder">
+      <div className="kpi-placeholder-div">
+        <h2 className="kpi-placeholder-title">KPI's</h2>
         <p>Coming soon... (this section will display your automation stats)</p>
       </div>
     </section>
--- frontend/src/components/dashboard/ServiceDetails.tsx
+++ frontend/src/components/dashboard/ServiceDetails.tsx
@@ -11,20 +11,20 @@ import type { ServiceCardProps } from "../../types";
 const ServiceDetails: React.FC<ServiceCardProps> = ({ service }) => {
     if (!service)
     return (
-        <div className="w-3/4 bg-white/5 border border-white/10 rounded-2xl p-6 text-center text-gray-400">
+        <div className="service-details service-details-no-service">
             No service selected
         </div>
     );
 
     return (
-    <div className="w-3/4 bg-white/5 border border-white/10 rounded-2xl p-6 text-white">
-      <h3 className="text-center text-3xl mb-4 font-bold text-indigo-400">{service.name}</h3>
+    <div className="service-details">
+      <h3 className="service-details-title">{service.name}</h3>
 
-      <div className="mb-6">
-        <h4 className="text-2xl font-semibold text-indigo-300 mb-2">
+      <div className="service-details-section">
+        <h4 className="service-details-section-title">
             Actions
         </h4>
-        <ul className="text-xl list-disc list-inside text-gray-300">
+        <ul className="service-details-list">
           {service.actions.map((a, i) => (
             <li key={i}>
               <strong>{a.name}</strong> — {a.description}
@@ -33,11 +33,11 @@ const ServiceDetails: React.FC<ServiceCardProps> = ({ service }) => {
         </ul>
       </div>
 
-      <div>
-        <h4 className="text-2xl font-semibold text-indigo-300 mb-2">
+      <div className="service-details-section">
+        <h4 className="service-details-section-title">
             Reactions
         </h4>
-        <ul className="text-xl list-disc list-inside text-gray-300">
+        <ul className="service-details-list">
           {service.reactions.map((r, i) => (
             <li key={i}>
               <strong>{r.name}</strong> — {r.description}
