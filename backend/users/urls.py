ad4573b84cc016b9bef04c956de7a17e7fe0cbb0
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-10-06 17:30:09 +0200

    feat(urls): add OAuth2 endpoints for service management

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,12 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from django.urls import path
 
+from .oauth_views import (
+    OAuthCallbackView,
+    OAuthInitiateView,
+    ServiceConnectionListView,
+    ServiceDisconnectView,
+)
 from .views import (
     EmailOrUsernameTokenObtainPairView,
     RegisterView,
@@ -11,6 +17,7 @@ from .views import (
 )
 
 urlpatterns = [
+    # Authentication endpoints
     path("register/", RegisterView.as_view(), name="auth_register"),
     path(
         "login/", EmailOrUsernameTokenObtainPairView.as_view(), name="token_obtain_pair"
@@ -23,4 +30,25 @@ urlpatterns = [
         name="send_verification_email",
     ),
     path("verify-email/<str:token>/", VerifyEmailView.as_view(), name="verify_email"),
+    # OAuth2 endpoints
+    path(
+        "oauth/<str:provider>/",
+        OAuthInitiateView.as_view(),
+        name="oauth_initiate",
+    ),
+    path(
+        "oauth/<str:provider>/callback/",
+        OAuthCallbackView.as_view(),
+        name="oauth_callback",
+    ),
+    path(
+        "services/",
+        ServiceConnectionListView.as_view(),
+        name="services_list",
+    ),
+    path(
+        "services/<str:provider>/disconnect/",
+        ServiceDisconnectView.as_view(),
+        name="service_disconnect",
+    ),
 ]
