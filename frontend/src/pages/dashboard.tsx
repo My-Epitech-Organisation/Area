099e49506595b52c8708b2481dfae9cdf2cf9d9a
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-10-06 20:27:41 +0200

    feat(dashboard): enhance layout and improve user greeting display

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,14 +1,48 @@
-import React from "react";
+/*
+** EPITECH PROJECT, 2025
+** Area
+** File description:
+** dashboard
+*/
+
+import React, {useState, useEffect} from "react";
+import type { Service, User } from "../types";
+import { mockServices } from "../data/mockServices";
+import { getStoredUser } from "../utils/helper";
+import { UserGreeting, ServiceSelector, ServiceDetails, KPIPlaceHolder } from "../components";
 
 const Dashboard: React.FC = () => {
+  // Example state to represent user data
+  //
+  const [user, setUser] = useState<User | null>(null);
+  const [services, setServices] = useState<Service[]>([]);
+  const [selectedService, setSelectedService] = useState<Service | null>(null);
+
+  useEffect(() => {
+    // Simulate fetching user data
+    setUser(getStoredUser());
+    setServices(mockServices);
+  }, []);
+
   return (
     <div className="w-screen min-h-screen bg-gradient-to-br from-black/90 via-gray-900/80 to-indigo-950 flex flex-col items-center">
-      <header className="w-full pt-28 flex justify-center">
-        <h1 className="text-5xl font-bold text-white">Dashboard</h1>
+      <header className="w-full flex flex-col pt-20 items-center mb-8">
+        <h1 className="text-6xl font-bold text-center mb-4">Dashboard</h1>
+        <UserGreeting user={user} />
       </header>
-      <main className="w-full flex-1 flex items-start justify-center">
-        <p className="text-lg text-gray-300 mt-10">Welcome to your Dashboard!</p>
-      </main>
+
+      {/* KPI placeholder */}
+      <KPIPlaceHolder />
+
+      {/* SERVICE SELECTOR */}
+        <ServiceSelector
+          services={services}
+          selectedService={selectedService}
+          onSelect={setSelectedService}
+        />
+
+      {/* SERVICE DETAILS */}
+        <ServiceDetails service={selectedService} />
     </div>
   );
 };
