51715318cb7ba5cdc07b372d80c79d283512f388
maelemiel <mael.perrigaud@epitech.eu>
2025-10-07 17:11:56 +0200

    refactor(create_applet_widgets): remove unnecessary library declaration for cleaner exports

--- mobile/lib/widgets/create_applet/create_applet_widgets.dart
+++ mobile/lib/widgets/create_applet/create_applet_widgets.dart
@@ -1,7 +1,3 @@
-/// Exports de tous les widgets de création d'applet
-/// Permet d'importer tous les composants en une seule ligne
-library create_applet_widgets;
-
 export 'automation_details_card.dart';
 export 'trigger_config_card.dart';
 export 'action_config_card.dart';
