7b9a52d7b9202b7fde585b6bbabba52d6bcba4f4
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-10-07 02:33:47 +0200

    refactor(settings): update default email address for consistency across environments

--- backend/area_project/settings.py
+++ backend/area_project/settings.py
@@ -263,7 +263,7 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@area.com")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@areaction.app")
 
 # Simple JWT Configuration
 SIMPLE_JWT = {
