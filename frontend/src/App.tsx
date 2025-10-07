64d2e16416192c07f4bead18f0da827a2212d6b0
Nolan Papa <nolan.papa@epitech.eu>
2025-10-07 14:16:39 +0200

    update(App.tsx): add routes for ServiceDetail and Areaction pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Services from "./pages/services";
+import ServiceDetail from "./pages/serviceDetail";
+import Areaction from "./pages/Areaction";
 import About from "./pages/about";
 import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/services/:serviceId" element={<ServiceDetail />} />
+        <Route path="/Areaction" element={<Areaction />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
