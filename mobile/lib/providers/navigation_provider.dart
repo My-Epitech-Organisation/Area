bb14cf94d966611b56831e11f55df9eda17f42ae
maelemiel <mael.perrigaud@epitech.eu>
2025-10-08 09:25:58 +0200

    feat(navigation_provider): add NavigationProvider for managing page navigation

---
+++ mobile/lib/providers/navigation_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class NavigationProvider extends ChangeNotifier {
+  int _currentPage = 0;
+  final PageController _pageController = PageController();
+
+  int get currentPage => _currentPage;
+  PageController get pageController => _pageController;
+
+  void navigateToPage(int pageIndex) {
+    if (pageIndex != _currentPage) {
+      _pageController.animateToPage(
+        pageIndex,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOutCubic,
+      );
+      _currentPage = pageIndex;
+      notifyListeners();
+    }
+  }
+
+  void updateCurrentPage(int pageIndex) {
+    _currentPage = pageIndex;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+}
