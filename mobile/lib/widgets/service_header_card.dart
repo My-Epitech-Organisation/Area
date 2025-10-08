9f1c164782a7e65f08f2ef893d40cb750602174c
maelemiel <mael.perrigaud@epitech.eu>
2025-10-08 10:51:50 +0200

    feat(mobile/flutter): implement ServiceIcons utility for consistent service icon retrieval

--- mobile/lib/widgets/service_header_card.dart
+++ mobile/lib/widgets/service_header_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/service.dart';
+import '../utils/service_icons.dart';
 
 class ServiceHeaderCard extends StatelessWidget {
   final Service service;
@@ -28,7 +29,7 @@ class ServiceHeaderCard extends StatelessWidget {
                 borderRadius: BorderRadius.circular(12),
               ),
               child: Icon(
-                _getServiceIcon(service.name),
+                ServiceIcons.getServiceIcon(service.name),
                 color: Theme.of(context).primaryColor,
                 size: 32,
               ),
@@ -58,38 +59,5 @@ class ServiceHeaderCard extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  IconData _getServiceIcon(String serviceName) {
-    switch (serviceName.toLowerCase()) {
-      case 'facebook':
-        return Icons.facebook;
-      case 'twitter':
-      case 'x':
-        return Icons.alternate_email;
-      case 'gmail':
-      case 'email':
-        return Icons.email;
-      case 'discord':
-        return Icons.chat;
-      case 'github':
-        return Icons.code;
-      case 'spotify':
-        return Icons.music_note;
-      case 'youtube':
-        return Icons.play_circle_fill;
-      case 'slack':
-        return Icons.message;
-      case 'telegram':
-        return Icons.send;
-      case 'weather':
-        return Icons.wb_sunny;
-      case 'timer':
-        return Icons.timer;
-      case 'rss':
-        return Icons.rss_feed;
-      default:
-        return Icons.apps;
-    }
   }
 }
