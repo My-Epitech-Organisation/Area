56d0b7fc23e847484be2bb85bb499fb0784fb028
Nolan Papa <nolan.papa@epitech.eu>
2025-10-08 09:21:41 +0200

    fix(routes): add catch-all route to redirect to Homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,6 +29,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   );
