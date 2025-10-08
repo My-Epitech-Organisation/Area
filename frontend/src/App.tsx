dacd74efd79794f6a2de058a699ae6cf325402b8
Nolan Papa <nolan.papa@epitech.eu>
2025-10-08 09:24:53 +0200

    fix(routes): update root route to use index for Homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   );
