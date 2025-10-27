291b8a14557d3b787bdbb7150fbb1093c4ae9124
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-10-27 21:28:32 +0100

    fix(homepage): update placeholder text in description paragraph

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -79,7 +79,7 @@ const Homepage: React.FC = () => {
                   AREA
                 </h2>
                 <p className="text-lg md:text-xl lg:text-3xl xl:text-5xl text-theme-secondary mt-4 px-4">
-                  &quot;Connect services, trigger actions: automate your routine and let your tools
+                  &quot;zfeeeeeeeeeeeeeeeeeeeeeeeee
                   work for you.&quot;
                 </p>
                 <div className="mt-6 md:mt-8">
