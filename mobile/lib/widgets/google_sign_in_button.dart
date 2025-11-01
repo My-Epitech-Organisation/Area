2c70fbde037a784a371eed3aa1b4a4868f3b8fb9
maelemiel <mael.perrigaud@epitech.eu>
2025-11-01 15:22:40 +0100

    fix(google_sign_in_button): replace image icon with SVG for improved rendering

--- mobile/lib/widgets/google_sign_in_button.dart
+++ mobile/lib/widgets/google_sign_in_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../config/service_provider_config.dart';
 
 class GoogleSignInButton extends StatelessWidget {
@@ -15,10 +16,12 @@ class GoogleSignInButton extends StatelessWidget {
     return SizedBox(
       height: 50,
       child: OutlinedButton.icon(
-        icon: Image.network(
-          ServiceProviderConfig.getIconUrl('google'),
+        icon: SvgPicture.network(
+          'https://www.gstatic.com/images/branding/product/1x/goog_onboarding_logo_2x_light.svg',
           height: 24,
           width: 24,
+          placeholderBuilder: (context) =>
+              const SizedBox(height: 24, width: 24),
         ),
         label: isLoading
             ? const SizedBox(
