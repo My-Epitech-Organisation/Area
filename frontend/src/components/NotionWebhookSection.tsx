51d09f97912cf9d9f1c5adb8d1659a1eab91ca15
Paul-Antoine SALMON <santiago.pidcova@epitech.eu>
2025-11-02 00:19:43 +0100

    refactor(notion): Remove unnecessary blank line in NotionWebhookSection component

--- frontend/src/components/NotionWebhookSection.tsx
+++ frontend/src/components/NotionWebhookSection.tsx
@@ -5,7 +5,6 @@
  * NotionWebhookSection - Component for displaying Notion webhook configuration
  */
 
-
 import React, { useState, useEffect } from 'react';
 import { API_BASE } from '../utils/helper';
 
